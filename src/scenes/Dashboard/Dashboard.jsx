01b3594207a06cbe6e6f71b1244e5e94837b3b2e
Jeff Anciado <jeffjustine7171@yahoo.com>
2023-01-16 14:45:51 +0800

    months data added

--- src/scenes/Dashboard/Dashboard.jsx
+++ src/scenes/Dashboard/Dashboard.jsx
@@ -22,7 +22,7 @@ class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
-      toggled: "Yearly",
+      toggled: "Monthly",
       toggles: ["Monthly", "Yearly"],
       month: "December",
       months: [
